0952836f9878d95860c7fac89023c0f1f5349b07
Arthur <tutsyyyyy.dpc16@gmail.com>
2026-06-13 22:36:01 -0500

    Remoção da exibição da quantidade de chaves

--- src/main/UI.java
+++ src/main/UI.java
@@ -9,7 +9,7 @@ public class UI {
 
     GamePanel gp;
     Font arial_40, arial_80B;
-    BufferedImage keyImage;
+    //BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter  = 0;
@@ -21,8 +21,8 @@ public class UI {
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-        OBJ_Key key = new OBJ_Key(gp);
-        keyImage = key.image;
+        //OBJ_Key key = new OBJ_Key(gp);
+        //keyImage = key.image;
     }
     public void showMessage(String text) {
 
@@ -31,53 +31,6 @@ public class UI {
     }
     public void draw(Graphics2D g2) {
 
-        if(gameFinished == true) {
-
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-
-            String text;
-            int textLength;
-            int x;
-            int y;
-
-            text = "You did!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - (gp.tileSize*3);
-            g2.drawString(text, x, y);
-
-            g2.setFont(arial_80B);
-            g2.setColor(Color.yellow);
-            text = "HAHAHAHA!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.tileSize*2);
-            g2.drawString(text, x, y);
-
-            gp.gameThread = null;
-
-        }
-        else {
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-            g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
-            g2.drawString("x " + gp.player.hasKey, 74, 65);
-
-            //MESSAGE
-            if(messageOn == true) {
-
-                g2.setFont(g2.getFont().deriveFont(30F));
-                g2.drawString(message, gp.tileSize*6, gp.tileSize*10);
-
-                messageCounter++;
-
-                if(messageCounter > 120) {
-
-                    messageCounter = 0;
-                    messageOn = false;
-                }
-            }
         }
 
 
